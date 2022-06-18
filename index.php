ad897430066150062a6caf78e5bda51c25a80af1
Sreign254 <102013887+Sreign254@users.noreply.github.com>
2022-06-18 13:08:01 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,257 @@
+
+
+
+<?php include 'sentMail.php';?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+<link rel="stylesheet" href="style.css">
+<script src="https://kit.fontawesome.com/332a215f17.js" cross origin="anonymous"></script>
+<link href="https://fonts.googleapis.com/css?family=Josefin+Sans:400,700&display=swap" rel="stylesheet">   
+<link rel="shortcut icon" href="./pmanager.jpg" type="image/svg+xml">
+<style>
+  .image { --ionicon-stroke-width: 40px;
+  border-radius: 50%; }
+</style>
+<title>pManager</title>
+  </head>
+  <body>
+     <div class="container-fluid icons py-2 px-3">
+         <div class="row">
+             <div class="col d-flex justify-content-between align-items-baseline">
+                
+                
+                 </div>
+                 <h6>noName</h6>
+             </div>
+         </div>
+     </div>
+      <header id="header">
+          <nav id="navbar" class="navbar navbar-expand-md">
+          <a class="navbar-brand px-2 py-0" href="#">pManager</a>
+           <button  class="navbar-toggler bg-light" type="button" 
+              data-toggle="collapse"  data-target="#navbar">
+           <div class="line1"></div>
+           <div class="line2"></div>
+           <div class="line3"></div>
+            </button>
+            <div class="collapse navbar-collapse justify-content-between" id="navbar">
+                <ul class="navbar-nav mx-auto">
+                    <li class="nav-item">
+                <a href="#" class="nav-link m-2 text-capitalize nav-active">Home</a>
+                    </li>
+                   
+                   
+                    <li class="nav-item">
+                        <a href="#" class="nav-link m-2 text-capitalize">Quotes</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link m-2 text-capitalize">Contact</a>
+                    </li>
+                </ul>
+                <form class="form-inline ml-3">
+                    <div class="input-group">
+                        <input type="text" class="form-control" 
+                        placeholder="search">
+                        <div class="input-group-append">
+                            <button class="btn search-btn" type="button">
+                                <i class="fas fa-search text-muted"></i>
+                            </button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+          </nav>
+          <!--Banner-->
+<section>
+    <div class="container-fluid">
+        <div class="row banner">
+            <div class="col-sm-10 heading">
+                <h1 class="display-2 text-uppercase">pManager<a>
+                <p>Helps keep track of your Project.</p>
+                <a href="../pj/html/login.php" class="btn btn-main btn-lg m-3">Team Leader</a>
+                <a href="../page/html/login.php" class="btn btn-main btn-lg m-3">Lead Engineer</a>
+            </div>
+
+        </div>
+    </div>
+</section>
+         <!--End of Banner-->
+         <!--About Section----->
+        
+         <!--End of About Section-->
+         <!---Projects----->
+       
+      </section>
+      <!--End of project Section-->
+       
+        
+      </section>
+         <!--End of Services-->
+         <!--Testimonial Section-->
+         <section id="testimonials" class="py-5 all">
+            <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+              <div class="carousel-inner">
+                <!--       carousel-item-->
+                <div class="carousel-item active">
+                  <div class="container">
+                    <div class="row">
+                      <div class="col-lg-10 mx-auto d-flex justify-content-between py-3">
+                        <div class="align-self-center ml-3">
+                          <img src="https://i.postimg.cc/NF6cCBYY/img-1.jpg" alt="" class=" rounded-circle main-img">
+                        </div>
+                        <div class="review-text px-5">
+                          <h2 class="text-capitalize mb-3 des-color">Steve Jobs</h2>
+                        <p class="text-light">
+                            Stay hungry,Stay Foolish..
+                         </p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <!--carousel-item-->
+                <!--       carousel-item-->
+                <div class="carousel-item">
+                  <div class="container">
+                    <div class="row">
+                      <div class="col-lg-10 mx-auto d-flex justify-content-between py-3">
+                        <div class="align-self-center ml-3">
+                          <img src="https://i.postimg.cc/QCGDqV20/img-2.jpg" alt="" class=" rounded-circle main-img">
+                        </div>
+                        <div class="review-text px-5">
+                          <h2 class="text-capitalize mb-3 des-color">Donald Duck</h2>
+        
+                          <p class="text-light">
+                           Be a yard stick of Character.
+                          </p>
+        
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <!--carousel-item-->
+                <br>
+                <div class="carousel-item">
+                  <div class="container">
+                    <div class="row">
+                      <div class="col-lg-10 mx-auto d-flex justify-content-between py-3">
+                        <div class="align-self-center ml-3">
+                          <img src="https://i.postimg.cc/sf7sg9W8/img-4.jpg" alt="" class=" rounded-circle main-img">
+                        </div>
+                        <div class="review-text px-5">
+                          <h2 class="text-capitalize mb-3 des-color">Bugs Bunny</h2>
+        
+                          <p class="text-light">
+                            We all make mistakes lets not be to specific.
+                            We try again tomorrow.
+        
+                          </p>
+        
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <!--carousel-item-->
+              </div>
+              <!-- carousel controls-->
+              <a class="carousel-control-prev" href="#carouselExampleControls" 
+              role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" ></span>
+            
+          </a>
+              <a class="carousel-control-next" href="#carouselExampleControls" 
+              role="button" data-slide="next">
+            <span class="carousel-control-next-icon" ></span>
+            
+          </a>
+            </div>
+          </section>
+       <!--End of Testimonial Section-->
+
+<!--Contact us section-->
+<section class="bg-color p-5">
+    <div class="container">
+        <div class="contact-us">
+            <div class="row">
+                <div class="col-md-11 text-center">
+                    <h2>Contact Us</h2>
+                    <span class="title-border"></span>
+                             <p>Help us improve our services. </p>
+                 </div>
+            </div>
+            <div class="row contact">
+               <!-- <div class="col-md-4 text-center">
+                    <i class="fas fa-mobile-alt fa-3x color"></i>
+                    <h5 class="mt-3">Phone</h5>
+                    <p>+55 61 12345678</p>
+                </div>
+                <div class="col-md-4 text-center">
+                    <i class="fas fa-map-marker-alt fa-3x color"></i>
+                    <h5 class="mt-3">Address</h5>
+                    <p>Lorem ipsum dolor sit amet.</p>
+                </div>
+                <div class="col-md-4 text-center">
+                    <i class="far fa-envelope-open fa-3x color"></i>
+                    <h5 class="mt-3">Email</h5>
+                    <p>info@info.com</p>
+                </div>-->
+            </div>
+            <div class="row justify-content-center">
+             <div class="col-lg-10 col-md-8 ">
+                <form>
+                    <div class="input-group input-group-lg mb-3">
+                      <div class="input-group-prepend">
+                        <span class="input-group-text">
+                          <i class="fas fa-user"></i>
+                        </span>
+                      </div>
+                      <input type="text" class="form-control" placeholder="Name">
+                    </div>
+        
+                    <div class="input-group input-group-lg mb-3">
+                      <div class="input-group-prepend">
+                        <span class="input-group-text">
+                          <i class="fas fa-envelope"></i>
+                        </span>
+                      </div>
+                      <input type="text" class="form-control" placeholder="Email">
+                    </div>
+        
+                    <div class="input-group input-group-lg mb-3">
+                      <div class="input-group-prepend">
+                        <span class="input-group-text">
+                          <i class="fas fa-pencil-alt"></i>
+                        </span>
+                      </div>
+                      <textarea class="form-control" placeholder="Message" rows="5"></textarea>
+                    </div>
+        
+                    <input type="submit" value="Submit" class="btn btn-cont btn-block btn-lg">
+                  </form>
+               
+                </div>
+
+            </div>
+
+        </div>
+    </div>
+</section>
+<!--end of contact us section-->
+
+<script src="script.js"></script>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+  </body>
+</html>
